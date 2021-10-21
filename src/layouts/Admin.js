a39ab3b9796f044443b3c1bb3ee76d1a08dada85
Brian Retamar <leo1632@hotmail.com>
2021-10-21 18:24:26 -0300

    updated

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -23,8 +23,8 @@ import { Container } from "reactstrap";
 import AdminNavbar from "../components/Navbars/AdminNavbar.js";
 import AdminFooter from "../components/Footers/AdminFooter.js";
 import Sidebar from "../components/Sidebar/Sidebar.js";
+import routes from "../routes-dashboard.js";
 
-import routes from "../routes.js";
 
 const Admin = (props) => {
   const mainContent = React.useRef(null);
