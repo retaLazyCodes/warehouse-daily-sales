d310a3a86625ee9c5ef58eb856a92f48d11ba638
Brian Retamar <leo1632@hotmail.com>
2021-11-22 20:55:29 -0300

    Showing products, with pagination and filters

--- src/components/Products/ProductsTable.js
+++ src/components/Products/ProductsTable.js
@@ -1,90 +1,60 @@
-import { useMemo } from 'react'
-import { useTable } from 'react-table'
+import MaterialTable from 'material-table'
+import swal from 'sweetalert2'
+import { tableIcons } from '../icons'
 
 function ProductsTable({ products }) {
     console.log(products)
 
-    const data = useMemo(
-        () => products,
-        []
-    )
+    const options = () => {
+        return `<option value="" selected disabled>-- Seleccione la categoría --</option>
+  <option value="1">Category 1</option>
+  <option value="2">Category 2</option>`
+    }
 
-    const columns = useMemo(
-        () => [
-            {
-                Header: 'Nombre',
-                accessor: 'name',
-            },
-            {
-                Header: 'Descripción',
-                accessor: 'description',
-            },
-            {
-                Header: 'Precio',
-                accessor: 'sale_price',
-            },
-            {
-                Header: 'Stock',
-                accessor: 'stock',
-            },
-        ],
-        []
-    )
+    async function showModal() {
 
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,
-    } = useTable({ columns, data })
+        const { value: formValues } = await swal.fire({
+            title: 'Crear producto',
+            showCancelButton: true,
+            html: `Nombre:<br/><input id="swal-input1" class="swal2-input">
+            <br/><br/>
+                    Descripción:<input id="swal-input2" class="swal2-input">
+            <br/><br/>
+      Categoría: <select id="swal-input3" class="swal2-select">${options()}</select>`,
+            focusConfirm: false,
+            preConfirm: () => {
+                return [
+                    document.getElementById('swal-input1').value,
+                    document.getElementById('swal-input2').value,
+                    document.getElementById('swal-input3').value
+                ]
+            }
+        })
+
+        if (formValues) {
+            console.log(formValues)
+            swal.fire(JSON.stringify(formValues))
+        }
+    }
+
+    showModal()
 
     return (
-        <table {...getTableProps()} style={{ border: 'solid 1px blue' }}>
-            <thead>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map(column => (
-                            <th
-                                {...column.getHeaderProps()}
-                                style={{
-                                    borderBottom: 'solid 3px red',
-                                    background: 'aliceblue',
-                                    color: 'black',
-                                    fontWeight: 'bold',
-                                }}
-                            >
-                                {column.render('Header')}
-                            </th>
-                        ))}
-                    </tr>
-                ))}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {rows.map(row => {
-                    prepareRow(row)
-                    return (
-                        <tr {...row.getRowProps()}>
-                            {row.cells.map(cell => {
-                                return (
-                                    <td
-                                        {...cell.getCellProps()}
-                                        style={{
-                                            padding: '10px',
-                                            border: 'solid 1px gray',
-                                            background: 'papayawhip',
-                                        }}
-                                    >
-                                        {cell.render('Cell')}
-                                    </td>
-                                )
-                            })}
-                        </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+        <div style={{ maxWidth: '100%' }}>
+            <MaterialTable
+                columns={[
+                    { title: 'Nombre', field: 'name' },
+                    { title: 'Descripción', field: 'description' },
+                    { title: 'Precio', field: 'sale_price' },
+                    { title: 'Stock', field: 'stock' }
+                ]}
+                data={products}
+                title="Lista de Productos"
+                icons={tableIcons}
+            />
+        </div>
     )
+
 }
 
 export default ProductsTable
